734c8b78bfbfccf587dc6857a1970ca2a11d7ab6
Dimov <dimov.d1988@gmail.com>
2020-05-14 23:58:30 +0300

    Initial Commit

---
+++ Stacking up buttons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stacking up buttons
+//
+//  Created by dimov_d on 14.05.2020.
+//  Copyright © 2020 dimov_d. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
